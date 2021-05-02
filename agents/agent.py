8d9957c4e672aaf3e729c806b0d53a3db0f0e471
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2021-05-02 19:39:43 -0300

    Completing agent file with modularization and .gitignore.
    Co-authored-by: Murilo Loiola <murilo.loiola.dan@gmail.com>

--- agents/agent.py
+++ agents/agent.py
@@ -1,22 +1,152 @@
-from pade.misc.utility import display_message, start_loop
-from pade.core.agent import Agent
-from pade.acl.aid import AID
-from pade.behaviours.protocols import TimedBehaviour
-from sys import argv
-from random import randint
+import time
+import numpy as np
+import pandas as pd
+import pylab as plt
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
 import enum
+from bokeh.io import show, output_notebook
+from bokeh.models import ColumnDataSource, GeoJSONDataSource, ColorBar, HoverTool, Legend, LinearColorMapper, ColorBar
+from bokeh.plotting import figure
+from bokeh.palettes import brewer
+from bokeh.models.glyphs import Line
+from bokeh.palettes import Category10, Viridis
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
 
 
-class States(enum.IntEnum):
+class State(enum.IntEnum):
     SUSCEPTIBLE = 0
     INFECTED = 1
-    REMOVED = 2
+    RECOVERED = 2
+    DECEASED = 3
 
-
-class Person(Agent):
-    def __init__(self, agent_id, model):
-        super(Person, self).__init__(agent_id)
-
-        self.age = randint(20, 40)
-        self.state = States.SUSCEPTIBLE
+class MyAgent(Agent):
+    """ An agent in an epidemic model."""
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.age = self.random.normalvariate(20,40)        
+        self.state = State.SUSCEPTIBLE  
         self.infection_time = 0
+
+    def move(self):
+        """Move the agent"""
+
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False)
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+    def status(self):
+        """Check infection status"""
+
+        if self.state == State.INFECTED:     
+            drate = self.model.death_rate
+            alive = np.random.choice([0,1], p=[drate,1-drate])
+            if alive == 0:
+                self.state = State.DECEASED
+                # self.model.schedule.remove(self)            
+            t = self.model.schedule.time-self.infection_time
+            if t >= self.recovery_time:          
+                self.state = State.DECEASED
+                # self.model.schedule.remove(self)
+            elif t >= 13:
+                if self.random.normalvariate(0,10) >= 5:
+                    self.state = State.RECOVERED
+
+    def contact(self):
+        """Find close contacts and infect"""
+
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])       
+        if len(cellmates) > 1:
+            for other in cellmates:
+                if self.random.random() > self.model.ptrans:
+                    continue
+                if self.state is State.INFECTED and other.state is State.SUSCEPTIBLE:                    
+                    other.state = State.INFECTED
+                    other.infection_time = self.model.schedule.time
+                    other.recovery_time = self.model.get_recovery_time()
+
+    def step(self):
+        self.status()
+        self.move()
+        self.contact()
+
+def get_column_data(model):
+    """pivot the model dataframe to get states count at each step"""
+    agent_state = model.datacollector.get_agent_vars_dataframe()
+    X = pd.pivot_table(agent_state.reset_index(),index='Step',columns='State',aggfunc=np.size,fill_value=0)    
+    labels = ['Susceptible','Infected','Recovered', 'Deceased']
+    X.columns = labels[:len(X.columns)]
+    return X
+
+
+def plot_states_bokeh(model,title=''):
+    """Plot cases per country"""
+
+    X = get_column_data(model)
+    X = X.reset_index()
+    source = ColumnDataSource(X)
+    i=0
+    colors = Category10[4]
+    items=[]
+    p = figure(plot_width=600,plot_height=400,tools=[],title=title,x_range=(0,100))        
+    for c in X.columns[1:]:
+        line = Line(x='Step',y=c, line_color=colors[i],line_width=3,line_alpha=.8,name=c)
+        glyph = p.add_glyph(source, line)
+        i+=1
+        items.append((c,[glyph]))
+
+    p.xaxis.axis_label = 'Step'
+    p.add_layout(Legend(location='center_right',   
+                items=items))
+    p.background_fill_color = "#e1e1ea"
+    p.background_fill_alpha = 0.5
+    p.legend.label_text_font_size = "10pt"
+    p.title.text_font_size = "15pt"
+    p.toolbar.logo = None
+    p.sizing_mode = 'scale_height' 
+    show(p)   
+    return p
+
+def grid_values(model):
+    """Get grid cell states"""
+
+    agent_counts = np.zeros((model.grid.width, model.grid.height))
+    w=model.grid.width
+    df=pd.DataFrame(agent_counts)
+    for cell in model.grid.coord_iter():
+        agents, x, y = cell
+        c=None
+        for a in agents:
+            c = a.state
+        df.iloc[x,y] = c
+    return df
+
+def plot_cells_bokeh(model):
+
+    agent_counts = np.zeros((model.grid.width, model.grid.height))
+    w=model.grid.width
+    df=grid_values(model)
+    df = pd.DataFrame(df.stack(), columns=['value']).reset_index()    
+    columns = ['value']
+    x = [(i, "@%s" %i) for i in columns]    
+    hover = HoverTool(
+        tooltips=x, point_policy='follow_mouse')
+    colors = Category10[3]
+    mapper = LinearColorMapper(palette=colors, low=df.value.min(), high=df.value.max())
+    p = figure(plot_width=500,plot_height=500, tools=[hover], x_range=(-1,w), y_range=(-1,w))
+    p.rect(x="level_0", y="level_1", width=1, height=1,
+       source=df,
+       fill_color={'field':'value', 'transform': mapper},
+       line_color='black')
+    p.background_fill_color = "black"
+    p.grid.grid_line_color = None    
+    p.axis.axis_line_color = None
+    p.toolbar.logo = None
+    # show(p)
+    return p
