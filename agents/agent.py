f249a67f9b4d0e4e5f6de49ae63be4fba5c2f477
murilo-dan <murilo.loiola.dan@gmail.com>
2021-05-03 01:52:56 -0300

    Further modularization.

--- agents/agent.py
+++ agents/agent.py
@@ -1,20 +1,11 @@
 import time
-import numpy as np
-import pandas as pd
 import pylab as plt
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 import enum
-from bokeh.io import show, output_notebook
-from bokeh.models import ColumnDataSource, GeoJSONDataSource, ColorBar, HoverTool, Legend, LinearColorMapper, ColorBar
-from bokeh.plotting import figure
-from bokeh.palettes import brewer
-from bokeh.models.glyphs import Line
-from bokeh.palettes import Category10, Viridis
-from mesa.visualization.modules import CanvasGrid
-from mesa.visualization.ModularVisualization import ModularServer
+import numpy as np
 
 
 class State(enum.IntEnum):
@@ -44,7 +35,7 @@ class MyAgent(Agent):
     def status(self):
         """Check infection status"""
 
-        if self.state == State.INFECTED:     
+        if self.state == State.INFECTED:
             drate = self.model.death_rate
             alive = np.random.choice([0,1], p=[drate,1-drate])
             if alive == 0:
@@ -75,78 +66,3 @@ class MyAgent(Agent):
         self.status()
         self.move()
         self.contact()
-
-def get_column_data(model):
-    """pivot the model dataframe to get states count at each step"""
-    agent_state = model.datacollector.get_agent_vars_dataframe()
-    X = pd.pivot_table(agent_state.reset_index(),index='Step',columns='State',aggfunc=np.size,fill_value=0)    
-    labels = ['Susceptible','Infected','Recovered', 'Deceased']
-    X.columns = labels[:len(X.columns)]
-    return X
-
-
-def plot_states_bokeh(model,title=''):
-    """Plot cases per country"""
-
-    X = get_column_data(model)
-    X = X.reset_index()
-    source = ColumnDataSource(X)
-    i=0
-    colors = Category10[4]
-    items=[]
-    p = figure(plot_width=600,plot_height=400,tools=[],title=title,x_range=(0,100))        
-    for c in X.columns[1:]:
-        line = Line(x='Step',y=c, line_color=colors[i],line_width=3,line_alpha=.8,name=c)
-        glyph = p.add_glyph(source, line)
-        i+=1
-        items.append((c,[glyph]))
-
-    p.xaxis.axis_label = 'Step'
-    p.add_layout(Legend(location='center_right',   
-                items=items))
-    p.background_fill_color = "#e1e1ea"
-    p.background_fill_alpha = 0.5
-    p.legend.label_text_font_size = "10pt"
-    p.title.text_font_size = "15pt"
-    p.toolbar.logo = None
-    p.sizing_mode = 'scale_height' 
-    show(p)   
-    return p
-
-def grid_values(model):
-    """Get grid cell states"""
-
-    agent_counts = np.zeros((model.grid.width, model.grid.height))
-    w=model.grid.width
-    df=pd.DataFrame(agent_counts)
-    for cell in model.grid.coord_iter():
-        agents, x, y = cell
-        c=None
-        for a in agents:
-            c = a.state
-        df.iloc[x,y] = c
-    return df
-
-def plot_cells_bokeh(model):
-
-    agent_counts = np.zeros((model.grid.width, model.grid.height))
-    w=model.grid.width
-    df=grid_values(model)
-    df = pd.DataFrame(df.stack(), columns=['value']).reset_index()    
-    columns = ['value']
-    x = [(i, "@%s" %i) for i in columns]    
-    hover = HoverTool(
-        tooltips=x, point_policy='follow_mouse')
-    colors = Category10[3]
-    mapper = LinearColorMapper(palette=colors, low=df.value.min(), high=df.value.max())
-    p = figure(plot_width=500,plot_height=500, tools=[hover], x_range=(-1,w), y_range=(-1,w))
-    p.rect(x="level_0", y="level_1", width=1, height=1,
-       source=df,
-       fill_color={'field':'value', 'transform': mapper},
-       line_color='black')
-    p.background_fill_color = "black"
-    p.grid.grid_line_color = None    
-    p.axis.axis_line_color = None
-    p.toolbar.logo = None
-    # show(p)
-    return p
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from agents.infectionmodel import InfectionModel
-from agents.agent import MyAgent, plot_cells_bokeh, plot_states_bokeh, get_column_data
+from agents.agent import MyAgent
+from agents.plot import plot_cells_bokeh, plot_states_bokeh, get_column_data
 import time
+from mesa.visualization.modules.ChartVisualization import ChartModule
 
 if __name__ == "__main__":
     steps=100
@@ -11,4 +13,4 @@ if __name__ == "__main__":
         plot_states_bokeh(model,title='step=%s' %i)
         p2=plot_cells_bokeh(model)
         print(get_column_data(model))
-        time.sleep(0.02)
+        time.sleep(10)
